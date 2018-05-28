48d7a56e2a4263d2c75c7eb4f22bdc83586a22e9
Elvis Veiga <elviswveiga@gmail.com>
2018-05-28 12:51:40 -0300

    Update WebApiConfig.cs
    Correção para retorno de JSON

--- PokemonAPI/App_Start/WebApiConfig.cs
+++ PokemonAPI/App_Start/WebApiConfig.cs
@@ -1,6 +1,8 @@
-﻿using System;
+using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Formatting;
 using System.Web.Http;
 
 namespace PokemonAPI
@@ -10,6 +12,9 @@ namespace PokemonAPI
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+            var jsonFormatter = config.Formatters.OfType<JsonMediaTypeFormatter>().First();
+            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
 
             // Web API routes
             config.MapHttpAttributeRoutes();
